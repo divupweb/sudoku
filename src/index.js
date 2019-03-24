94dcebee6d3e4f7468312e81db2fca849c35156b
divupweb <divupweb@gmail.com>
2019-03-24 14:36:49 +0300

    feat: add final decigion

--- src/index.js
+++ src/index.js
@@ -1,26 +1,34 @@
 module.exports = function solveSudoku(matrix) {
-    for(let row=0;row<matrix.length;row++)
-    for(let col=0;col<matrix.length;col++){
-        if(matrix[row][col]==0){
-            let solutionScope = findSolution(row,col,matrix);
-            for(let solution of solutionScope){
-                matrix[row][col] = solution;
-                solveSudoku(matrix);
+    const matrixDeepCopy = JSON.parse(JSON.stringify(matrix));
+    for(let row=0;row<9;row++)
+    for(let col=0;col<9;col++){
+        if(matrixDeepCopy[row][col]===0){
+            let solutionScope = findSolution(row,col,matrixDeepCopy);
+            if(solutionScope.length==0){
+              return false;
             }
+            for(let solution of solutionScope){
+                matrixDeepCopy[row][col] = solution;
+                var result = solveSudoku(matrixDeepCopy);
+                if(result!=false){
+                  return solveSudoku(result);
+                }
+            }
+            return false;
         }
     }
-    return matrix;
+    return  matrixDeepCopy;
 }
 
 function findSolution(row,col,matrix){
-  let scope = [];
-  label: for(let i=1;i<10;i++){
-      for(let j=0;j<matrix.length;j++){
-          if(matrix[row][j]==i || matrix[j][col]==i || matrix[Math.floor(row/3)*3+j%3][Math.floor(col/3)*3+Math.floor(j/3)]==i){
-              continue label;
-          }
-      }
-      scope.push(i);
-  }
-return scope;
+    const scope = [];
+    label: for(let i=1;i<10;i++){
+        for(let j=0;j<9;j++){
+            if(matrix[row][j]==i || matrix[j][col]==i || matrix[Math.floor(row/3)*3+j%3][Math.floor(col/3)*3+Math.floor(j/3)]==i){
+                continue label;
+            }
+        }
+        scope.push(i);
+    }
+  return scope;
 }
